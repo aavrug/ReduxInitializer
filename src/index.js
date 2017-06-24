e4e0abb1b099a6dc697a8216328be64abff2f39b
Gaurav Kumar <aavrug@gmail.com>
2017-06-25 01:16:21 +0530

    Added youtube video search.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+
+const API_KEY = 'Your developers app API KEY';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+});
 
 const App = () => {
 	return (
