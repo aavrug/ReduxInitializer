43935082a56b6d143947a5b156b516c39cc719d3
Gaurav Kumar <aavrug@gmail.com>
2017-06-25 00:51:33 +0530

    Added one line call for event handler as per ES6 syntax.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 	render() {
-		return <input onChange={this.handleInputChange} />
-	}
-
-	handleInputChange() {
-		console.log('here');
+		return <input onChange={event => {console.log(event.target.value)}} />
 	}
 }
 
