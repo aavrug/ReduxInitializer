245c27c0955fe2f0f43ef08608c13c231eeb0c42
Gaurav Kumar <aavrug@gmail.com>
2017-06-25 18:32:29 +0530

    Added displaying books list using redux.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	return [
+		{title: 'Laravel- PHP framework'},
+		{title: 'The Inception'},
+		{title: 'Eloquent'},
+		{title: 'Harry Potter'},
+		{title: 'The Unknown Story'}
+	];
+};
